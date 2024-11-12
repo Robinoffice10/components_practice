9799b656cca8d1121dc73ef94a69578faaa302e0
Robin_Dhillon <robinoffice10@gmail.com>
2024-11-12 13:17:37 +0530

    firsty

---
+++ src/components/header/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="header">
+        <title>React first</title>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
